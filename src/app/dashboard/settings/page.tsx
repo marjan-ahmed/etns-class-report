9a6ca619ac5f98e2ce6e0eca989fec84b8439751
marjan-ahmed <marjan.ahmed08@yahoo.com>
2024-11-25 17:06:32 +0500

    "settings" page added

---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Settings() {
+  return (
+    <div className='p-10'>
+      <div className='flex flex-wrap justify-between items-center'>
+        <h2 className='font-bold text-2xl'>Settings</h2>
+        </div>
+        </div>
+  )
+}
+
+export default Settings
